f334cb30832dde7b53057c1d4c27ce5455fea46f
jalil <jalil@1001.digital>
2021-08-13 01:33:23 +0200

    Move test contracts

--- contracts/test/CallFromConstructor.sol
+++ contracts/test/CallFromConstructor.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./CheckAddress.sol";
+import "./../CheckAddress.sol";
 
 /// @title A dummy contract in order to test whether the library
 /// works when called from within a contract constructor.
